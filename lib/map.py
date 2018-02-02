8d62f59b4af8c86bb5e6a83f5eefeb8cbc13c97a
Victor Alves <victor@sub.pro.br>
2018-02-02 20:58:23 +0100

    Add change to optimize map rendering and work in progress with topographic view with colored pixels

--- lib/map.py
+++ lib/map.py
@@ -1,11 +1,39 @@
 import pygame
+import re
+
 from random import shuffle, randrange
+from lib.imageloader import imageLoader
+
+
+class MapBlock():
+
+    def __init__(self, image):
+        self.image = self.asset = image
+        self.rect = image.get_rect()
+
+
+class MapBlockImage(MapBlock):
+
+    def __init__(self, image, scale, area):
+        image = imageLoader(image, scale, area)
+        image.set_colorkey(self.image.get_at((0, 0)))
+        super().__init__(image)
+
+
+class MapBlockColor(MapBlock):
+
+    def __init__(self, color, tileSize):
+        surface = pygame.Surface(tileSize)
+        surface.fill(color)
+        super().__init__(surface)
+
+
 class MapGrid():
 
     def __init__(self, mapSize, tileSize, columns, rows):
         self.image = self.asset = pygame.Surface(mapSize)
         self.image.set_colorkey(self.image.get_at((0, 0)))
-        # self.image.set_alpha(90)
+        self.image.set_alpha(60)
 
         white = (255, 255, 255)
         x, y = (0, 1)
@@ -25,28 +53,42 @@ class MapGrid():
 
 
 class Map(pygame.sprite.Sprite):
-    title = {
-        ".": (255, 188, 108),
-        "X": (252, 141, 0),
-    }
 
     def __init__(self, viewSize, tileSize):
+        self.map = None
+        self.minimap = None
+        self.minimapTile = (2, 2)
         self.mapArray = []
+        self.mapArrayAsset = []
         self.mapSize = (0, 0)
         self.tileSize = tileSize
         self.viewSize = viewSize
         self.currentViewPosition = (0, 0)
         self.viewPosition = (0, 0)
-        self.viewUpdate = False
+        self.viewUpdate = True
+
+        self.minitile = {
+            ".": MapBlockColor((255, 188, 108), self.minimapTile),
+            "a": MapBlockColor((30, 40, 32), self.minimapTile),
+            "b": MapBlockColor((60, 141, 90), self.minimapTile),
+            "c": MapBlockColor((97, 162, 107), self.minimapTile),
+        }
+        self.tile = {
+            '.': MapBlockColor((231, 212, 169), self.tileSize),
+            'a': MapBlockColor((30, 40, 32), self.tileSize),
+            'b': MapBlockColor((60, 141, 90), self.tileSize),
+            'c': MapBlockColor((97, 162, 107), self.tileSize),
+            # "X": MapBlockImage("images/ground.png", 1, (23 * 2, 23 * 1, 23, 23)),
+        }
+
         self.generateMazeMap()
         self.loadMap()
 
     def generateMazeMap(self):
-        w = 5
-        h = 15
+        w, h = (5, 15)
         vis = [[0] * w + [1] for _ in range(h)] + [[1] * (w + 1)]
-        ver = [["X......"] * w + ['X'] for _ in range(h)] + [[]]
-        hor = [["XXXXXXX"] * w + ['X'] for _ in range(h + 1)]
+        ver = [["abc      "] * w + ['c'] for _ in range(h)] + [[]]
+        hor = [["cbaaaaabc"] * w + ['c'] for _ in range(h + 1)]
 
         def walk(x, y):
             vis[y][x] = 1
@@ -57,87 +99,84 @@ class Map(pygame.sprite.Sprite):
                 if vis[yy][xx]:
                     continue
                 if xx == x:
-                    hor[max(y, yy)][x] = "X......"
+                    hor[max(y, yy)][x] = "abc      "
                 if yy == y:
-                    ver[y][max(x, xx)] = "......."
+                    ver[y][max(x, xx)] = "         "
                 walk(xx, yy)
 
         walk(randrange(w), randrange(h))
 
         s = "\n"
         for (a, b) in zip(hor, ver):
-            s += ''.join(
+
+            # walls = a
+            # re.sub(r'X(X+)X', repl, string)
+            section = ''.join(
                 a + ['\n'] +
                 b + ['\n'] +
                 b + ['\n'] +
                 b + ['\n'] +
                 b + ['\n'] +
                 b + ['\n'] +
-                b + ['\n'] +
                 b + ['\n'])
+
+            s += section
+
+        print("block: \n", s)
         self.fullMap = s
 
     def loadMap(self):
-        x = 0
-        y = 1
+        x, y = (0, 1)
         mapYs = [m for m in self.fullMap.split("\n")[:-1]]
         self.mapArray = [[my for my in m] for m in mapYs[1:]]
+        self.minimapView = (
+            self.minimapTile[x] * len(self.mapArray[0]),
+            self.minimapTile[y] * (len(self.mapArray) - 6))  # magic num :-P
 
         fullMapWidth = self.tileSize[x] * len(self.mapArray[0])
-        fullMapHeight = self.tileSize[y] * len(mapYs[1:])
+        fullMapHeight = self.tileSize[y] * len(self.mapArray)
         self.fullMapSize = (fullMapWidth, fullMapHeight)
 
         # We need only what we can fit on the screen
         mapWidth = self.fullMapSize[x] - self.viewSize[x]
         mapHeight = self.fullMapSize[y] - self.viewSize[y]
         self.mapSize = (mapWidth, mapHeight)
+        self.loadMinimap()
+        self.drawMap()
 
     def setViewSize(self, viewSize):
-        x = 0
-        y = 1
+        x, y = (0, 1)
         mapWidth = self.fullMapSize[x] - viewSize[x]
         mapHeight = self.fullMapSize[y] - viewSize[y]
         self.viewSize = viewSize
         self.mapSize = (mapWidth, mapHeight)
 
     def updated(self):
-        x = 0
-        y = 1
+        x, y = (0, 1)
         return (self.currentViewPosition[x] != self.viewPosition[x] or
                 self.currentViewPosition[y] != self.viewPosition[y] or
                 self.viewUpdate)
 
-    def drawMinimap(self):
-        self.miniMapView = (72, 242)
-        mapSurface = pygame.Surface(self.miniMapView)
+    def loadMinimap(self):
+        x, y = (0, 1)
+        # self.minimapView = (72, 242)
+        # self.miniMapTile = (2, 2)
+        self.minimap = pygame.Surface(self.minimapView)
+        self.minimap.fill((231, 212, 169))
+        # self.minimap.set_colorkey((0, 0, 0))
 
         for yidx, xmap in enumerate(self.mapArray):
-            moveY = (2 * yidx)
+            moveY = (self.minimapTile[y] * yidx)
 
             for xidx, xym in enumerate(xmap):
-                moveX = (2 * xidx)
-
-                terrain = pygame.Rect(
-                    moveX, moveY, 2, 2)
-
-                pygame.draw.rect(mapSurface, self.title[xym], terrain)
-
-                if moveX >= self.viewSize[0] and moveY >= self.viewSize[1]:
-                    return mapSurface
-
-        for yidx, xmap in enumerate(self.mapArray):
-            for xidx, xym in enumerate(xmap):
-                terrain = pygame.Rect(0, 0, 2, 2)
-
-                pygame.draw.rect(mapSurface, self.title[xym], terrain)
-
-        return mapSurface
+                moveX = (self.minimapTile[x] * xidx)
+                if xym is not " ":
+                    self.minimap.blit(self.minitile[xym].image, (moveX, moveY))
 
     def drawMap(self):
-        x = 0
-        y = 1
-        self.reset()
-        mapSurface = pygame.Surface(self.viewSize)
+        x, y = (0, 1)
+        self.map = pygame.Surface(self.viewSize)
+        self.map.fill((231, 212, 169))
 
         viewX = int(self.viewPosition[x] / self.tileSize[x])
         viewY = int(self.viewPosition[y] / self.tileSize[y])
@@ -149,17 +188,16 @@ class Map(pygame.sprite.Sprite):
             for xidx, xym in enumerate(xmap[viewX:]):
                 xidx += viewX
                 moveX = (self.tileSize[x] * xidx) - self.viewPosition[x]
-
-                terrain = pygame.Rect(
-                    moveX, moveY, self.tileSize[x], self.tileSize[y])
-
-                pygame.draw.rect(mapSurface, self.title[xym], terrain)
+                if xym is not " ":
+                    self.map.blit(self.tile[xym].image, (moveX, moveY))
 
                 if moveX >= self.viewSize[x] and moveY >= self.viewSize[y]:
-                    return mapSurface
+                    return
 
-        return mapSurface
+    def refresh(self):
+        self.viewUpdate = True
+        self.drawMap()
 
-    def reset(self):
+    def onUpdate(self):
         self.viewUpdate = False
         self.currentViewPosition = self.viewPosition
